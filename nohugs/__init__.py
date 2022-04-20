346a8e61a42643023c95f6f6ead01434059e1fce
DJTOMATO <48269777+DJTOMATO@users.noreply.github.com>
2022-04-19 20:41:14 -0400

    Added disclaimer

--- nohugs/__init__.py
+++ nohugs/__init__.py
@@ -1,3 +1,5 @@
+# Please note that cog load order is not guaranteed, so it's not guaranteed to work every time, even thought my initial tests and reboots seemed to work. Just keep this in mind
+
 def setup(bot):
     cmds = ["hug"]
     for cmd_name in cmds:
