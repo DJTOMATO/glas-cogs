9e7a494437d2f3c7a61561f711fb02c5f814823a
DJTOMATO <48269777+DJTOMATO@users.noreply.github.com>
2022-06-24 01:44:08 -0400

    Fumopic
    initial test

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -99,6 +99,23 @@ class PfpImgen(commands.Cog):
 
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.command(aliases=["fumopicture"], cooldown_after_parsing=True)
+    async def fumopic(self, ctx, *, member: FuzzyMember = None):
+        """Make a fumopic avatar..."""
+        if not member:
+            member = ctx.author
+
+        async with ctx.typing():
+            avatar = await self.get_avatar(member)
+            task = functools.partial(self.gen_fumopic, ctx, avatar)
+            image = await self.generate_image(ctx, task)
+        if isinstance(image, str):
+            await ctx.send(image)
+        else:
+            await ctx.send(file=image)
+
+    @commands.bot_has_permissions(attach_files=True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
     @commands.command(aliases=["nofunallowed"], cooldown_after_parsing=True)
     async def nofun(self, ctx, *, member: FuzzyMember = None):
         """Make a nofun avatar..."""
@@ -817,5 +834,29 @@ class PfpImgen(commands.Cog):
         fp.seek(0)
         im.close()
         _file = discord.File(fp, "you.png")
+        fp.close()
+        return _file
+
+    def fumopic_you(self, ctx, member_avatar):
+        member_avatar = self.bytes_to_image(member_avatar, 130)
+        # base canvas
+        im = Image.new("RGBA", (451, 600), None)
+
+        fumopicmask = Image.open(
+            f"{bundled_data_path(self)}/fumopic/fumopic_mask.png", mode="r"
+        ).convert("RGBA")
+        # im.paste(you, (0, 0), you)
+
+        # pasting the pfp
+        im.paste(member_avatar, (280, 140), member_avatar)
+        im.paste(fumopicmask, (0, 0), fumopicmask)
+        fumopicmask.close()
+        member_avatar.close()
+
+        fp = BytesIO()
+        im.save(fp, "PNG")
+        fp.seek(0)
+        im.close()
+        _file = discord.File(fp, "fumopic.png")
         fp.close()
         return _file
