3e81477e68261edc142ab52ade843279cd8377a3
DJTOMATO <48269777+DJTOMATO@users.noreply.github.com>
2022-08-02 01:32:07 -0400

    Marisa 1.0

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -133,6 +133,23 @@ class PfpImgen(commands.Cog):
 
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.command(aliases=["gos"], cooldown_after_parsing=True)
+    async def marisa(self, ctx, *, member: FuzzyMember = None):
+        """Make a Marisa avatar..."""
+        if not member:
+            member = ctx.author
+
+        async with ctx.typing():
+            avatar = await self.get_avatar(member)
+            task = functools.partial(self.gen_marisa, ctx, avatar)
+            image = await self.generate_image(ctx, task)
+        if isinstance(image, str):
+            await ctx.send(image)
+        else:
+            await ctx.send(file=image)
+
+    @commands.bot_has_permissions(attach_files=True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
     @commands.command(aliases=["inapic"], cooldown_after_parsing=True)
     async def ina(self, ctx, *, member: FuzzyMember = None):
         """Make a Ina avatar..."""
@@ -946,5 +963,31 @@ class PfpImgen(commands.Cog):
         fp.seek(0)
         im.close()
         _file = discord.File(fp, "gosling.png")
+        fp.close()
+        return _file
+
+    def gen_marisa(self, ctx, member_avatar):
+        member_avatar = self.bytes_to_image(member_avatar, 300)
+        # base canvas
+        im = Image.new("RGBA", (433, 577), None)
+
+        marisamask = Image.open(
+            f"{bundled_data_path(self)}/marisa/marisa_mask.png", mode="r"
+        ).convert("RGBA")
+        # im.paste(you, (0, 0), you)
+
+        # pasting the pfp
+
+        im.rotate(120, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+        im.paste(member_avatar, (0, 0), member_avatar)
+        im.paste(marisamask, (0, 0), marisamask)
+        marisamask.close()
+        member_avatar.close()
+
+        fp = BytesIO()
+        im.save(fp, "PNG")
+        fp.seek(0)
+        im.close()
+        _file = discord.File(fp, "marisa.png")
         fp.close()
         return _file
