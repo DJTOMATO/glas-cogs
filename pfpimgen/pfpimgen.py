9e5d8d684df03456f1c3f051ad66c1b14f630e54
DJTOMATO <48269777+DJTOMATO@users.noreply.github.com>
2022-08-02 01:33:33 -0400

    Quick fix for marisa

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -133,7 +133,7 @@ class PfpImgen(commands.Cog):
 
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
-    @commands.command(aliases=["gos"], cooldown_after_parsing=True)
+    @commands.command(aliases=["selfie"], cooldown_after_parsing=True)
     async def marisa(self, ctx, *, member: FuzzyMember = None):
         """Make a Marisa avatar..."""
         if not member:
