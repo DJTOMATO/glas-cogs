155378c367ef53c9207847a39e804ebc4e8cb57b
DJTOMATO <48269777+DJTOMATO@users.noreply.github.com>
2022-08-21 13:59:50 -0400

    Update pfpimgen.py

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -1103,7 +1103,7 @@ class PfpImgen(commands.Cog):
             "RGBA"
         )
         im.rotate(120, resample=0, expand=0, center=None, translate=None, fillcolor=None)
-        im.paste(member_avatar, (10, 0), member_avatar)
+        im.paste(member_avatar, (0, 0), member_avatar)
         im.paste(liesmask, (0, 0), liesmask)
         liesmask.close()
         member_avatar.close()
