abcd748d356f541b1fe626700dbf2b72770e1919
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-03-01 15:42:26 -0300

    it is

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -568,6 +568,23 @@ class PfpImgen(commands.Cog):
             await ctx.send(image)
         else:
             await ctx.send(file=image)
+            
+    @commands.bot_has_permissions(attach_files=True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.command(cooldown_after_parsing=True)
+    async def itis(self, ctx, *, member: FuzzyMember = None):
+        """You are running out..."""
+        if not member:
+            member = ctx.author
+
+        async with ctx.typing():
+            avatar = await self.get_avatar(member)
+            task = functools.partial(self.gen_itis, ctx, avatar)
+            image = await self.generate_image(ctx, task)
+        if isinstance(image, str):
+            await ctx.send(image)
+        else:
+            await ctx.send(file=image)
 
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
@@ -1379,5 +1396,31 @@ class PfpImgen(commands.Cog):
         fp.seek(0)
         im.close()
         _file = discord.File(fp, "dream.png")
+        fp.close()
+        return _file
+
+    def gen_itis(self, ctx, member_avatar):
+        member_avatar = self.bytes_to_image(member_avatar, 486)
+
+        # member_avatar = member_avatar.rotate(330, Image.NEAREST, expand=1)
+        # base canvas
+        im = Image.new("RGBA", (574, 485), None)
+        itismask = Image.open(
+            f"{bundled_data_path(self)}/itis/itis_mask.png", mode="r"
+        ).convert("RGBA")
+
+        # member_avatar.rotate(90, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+        # im.rotate(120, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+
+        im.paste(member_avatar, (0, 0), member_avatar)
+        im.paste(itismask, (0, 0), itismask)
+        itismask.close()
+        member_avatar.close()
+
+        fp = BytesIO()
+        im.save(fp, "PNG")
+        fp.seek(0)
+        im.close()
+        _file = discord.File(fp, "itis.png")
         fp.close()
         return _file
