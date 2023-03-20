ddd53a09fc0525735dfdfdb5c61303181d20edfe
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-03-20 02:35:31 -0300

    Update movietar.py

--- movietar/movietar.py
+++ movietar/movietar.py
@@ -94,12 +94,11 @@ class Movietar(commands.Cog):
         #masked_clip = clip.fx(mpe.vfx.mask_color, color=[0, 1, 0], s=3)
         final_clip = mpe.CompositeVideoClip([member_avatar, clip]).set_duration(
             clip.duration
-        )
+        ).set_pos(("0","0")))
 
         final_clip.write_videofile(f"{bundled_data_path(self)}/clip.mp4")
         video = mpe.VideoFileClip(f"{bundled_data_path(self)}/clip.mp4")
         image = member_avatar
         final = mpe.CompositeVideoClip([image, video.set_position("center")])
         final.write_videofile("test.mp4")
-        video = mpe.
         return final
