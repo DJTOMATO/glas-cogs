8863daa5e9508032796e58bec02f92578530501e
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-03-23 22:58:57 -0300

    Update youwoulnt.py

--- youwoulnt/youwoulnt.py
+++ youwoulnt/youwoulnt.py
@@ -110,7 +110,7 @@ class Youwoulnt(commands.Cog):
         font = ImageFont.truetype(f"{bundled_data_path(self)}/xband-ro.ttf", 90)
         canvas = ImageDraw.Draw(im)
         
-        margin = offset = 40
+        margin = offset = 90
         for line in textwrap.wrap(text, width=40):
             canvas.text((margin, offset), line, font=font, fill="#FFFFFF", align="center", stroke_width=1, stroke_fill=(169,169,169))
             offset += font.getsize(line)[1]       
