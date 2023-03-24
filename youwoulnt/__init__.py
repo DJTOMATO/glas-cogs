7d14bb17f7a60608807dbfd4d0a66943258444dd
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-03-23 22:23:04 -0300

    Update __init__.py

--- youwoulnt/__init__.py
+++ youwoulnt/__init__.py
@@ -27,12 +27,12 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .youwoulnt import PfpImgen
+from .youwoulnt import Youwoulnt
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 def setup(bot: Red):
-    cog = youwoulnt(bot)
+    cog = Youwoulnt(bot)
     bot.add_cog(cog)
