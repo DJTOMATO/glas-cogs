034e30e0e2ce00aee8f440a893ceee0110d195ed
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-03-28 15:48:56 -0300

    [PFPIMGEN] My honest reaction

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -620,6 +620,23 @@ class PfpImgen(commands.Cog):
             await ctx.send(image)
         else:
             await ctx.send(file=image)
+            
+    @commands.bot_has_permissions(attach_files=True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.command(cooldown_after_parsing=True)
+    async def mhr(self, ctx, *, member: FuzzyMember = None):
+        """My honest reaction..."""
+        if not member:
+            member = ctx.author
+
+        async with ctx.typing():
+            avatar = await self.get_avatar(member)
+            task = functools.partial(self.gen_mhr, ctx, avatar)
+            image = await self.generate_image(ctx, task)
+        if isinstance(image, str):
+            await ctx.send(image)
+        else:
+            await ctx.send(file=image)
 
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
@@ -1331,7 +1348,7 @@ class PfpImgen(commands.Cog):
         return _file
     
     def gen_funado(self, ctx, member_avatar):
-        member_avatar = self.bytes_to_image(member_avatar, 500)
+        member_avatar = self.bytes_to_image(member_avatar, 450)
 
         im = Image.new("RGBA", (960, 958), None)
         funamask = Image.open(
@@ -1339,7 +1356,7 @@ class PfpImgen(commands.Cog):
         ).convert("RGBA")
 
         im.paste(funamask, (0, 0), funamask)
-        im.paste(member_avatar, (230, 30), member_avatar)
+        im.paste(member_avatar, (220, 30), member_avatar)
         
         funamask.close()
         member_avatar.close()
@@ -1455,5 +1472,31 @@ class PfpImgen(commands.Cog):
         fp.seek(0)
         im.close()
         _file = discord.File(fp, "itis.png")
+        fp.close()
+        return _file
+
+    def gen_mhr(self, ctx, member_avatar):
+        member_avatar = self.bytes_to_image(member_avatar, 640)
+
+        # member_avatar = member_avatar.rotate(330, Image.NEAREST, expand=1)
+        # base canvas
+        im = Image.new("RGBA", (720, 720), None)
+        mhrmask = Image.open(f"{bundled_data_path(self)}/mhr/mhr_mask.png", mode="r").convert(
+            "RGBA"
+        )
+
+        # member_avatar.rotate(90, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+        # im.rotate(120, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+
+        im.paste(member_avatar, (0, 80), member_avatar)
+        im.paste(mhrmask, (0, 0), mhrmask)
+        mhrmask.close()
+        member_avatar.close()
+
+        fp = BytesIO()
+        im.save(fp, "PNG")
+        fp.seek(0)
+        im.close()
+        _file = discord.File(fp, "mhr.png")
         fp.close()
         return _file
