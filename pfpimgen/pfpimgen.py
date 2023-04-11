fd56a9c620ea14df2ab46628882525dc0e98c272
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-04-10 22:04:32 -0400

    added slur

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -624,6 +624,23 @@ class PfpImgen(commands.Cog):
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
     @commands.command(cooldown_after_parsing=True)
+    async def slur(self, ctx, *, member: FuzzyMember = None):
+        """TIME TO GO CALL SOMEONE A SLUR ON DISCORD!..."""
+        if not member:
+            member = ctx.author
+
+        async with ctx.typing():
+            avatar = await self.get_avatar(member)
+            task = functools.partial(self.gen_slur, ctx, avatar)
+            image = await self.generate_image(ctx, task)
+        if isinstance(image, str):
+            await ctx.send(image)
+        else:
+            await ctx.send(file=image)
+            
+    @commands.bot_has_permissions(attach_files=True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.command(cooldown_after_parsing=True)
     async def mhr(self, ctx, *, member: FuzzyMember = None):
         """My honest reaction..."""
         if not member:
@@ -1475,6 +1492,32 @@ class PfpImgen(commands.Cog):
         fp.close()
         return _file
 
+    def gen_slur(self, ctx, member_avatar):
+        member_avatar = self.bytes_to_image(member_avatar, 600)
+
+        # member_avatar = member_avatar.rotate(330, Image.NEAREST, expand=1)
+        # base canvas
+        im = Image.new("RGBA", (600, 600), None)
+        slurmask = Image.open(f"{bundled_data_path(self)}/slur/slur_mask.png", mode="r").convert(
+            "RGBA"
+        )
+
+        # member_avatar.rotate(90, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+        # im.rotate(120, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+
+        im.paste(member_avatar, (0, 0), member_avatar)
+        im.paste(slurmask, (0, 0), slurmask)
+        slurmask.close()
+        member_avatar.close()
+
+        fp = BytesIO()
+        im.save(fp, "PNG")
+        fp.seek(0)
+        im.close()
+        _file = discord.File(fp, "slur.png")
+        fp.close()
+        return _file
+    
     def gen_mhr(self, ctx, member_avatar):
         member_avatar = self.bytes_to_image(member_avatar, 650)
 
