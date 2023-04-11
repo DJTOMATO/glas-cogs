856a57143582b04ac08493ec7e3d585476e80582
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-04-10 22:10:17 -0400

    Added jar

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -641,6 +641,23 @@ class PfpImgen(commands.Cog):
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
     @commands.command(cooldown_after_parsing=True)
+    async def jar(self, ctx, *, member: FuzzyMember = None):
+        """To the jar!..."""
+        if not member:
+            member = ctx.author
+
+        async with ctx.typing():
+            avatar = await self.get_avatar(member)
+            task = functools.partial(self.gen_jar, ctx, avatar)
+            image = await self.generate_image(ctx, task)
+        if isinstance(image, str):
+            await ctx.send(image)
+        else:
+            await ctx.send(file=image)
+            
+    @commands.bot_has_permissions(attach_files=True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.command(cooldown_after_parsing=True)
     async def mhr(self, ctx, *, member: FuzzyMember = None):
         """My honest reaction..."""
         if not member:
@@ -1515,6 +1532,32 @@ class PfpImgen(commands.Cog):
         fp.seek(0)
         im.close()
         _file = discord.File(fp, "slur.png")
+        fp.close()
+        return _file
+    
+    def gen_jar(self, ctx, member_avatar):
+        member_avatar = self.bytes_to_image(member_avatar, 600)
+
+        # member_avatar = member_avatar.rotate(330, Image.NEAREST, expand=1)
+        # base canvas
+        im = Image.new("RGBA", (600, 600), None)
+        jarmask = Image.open(f"{bundled_data_path(self)}/jar/jar_mask.png", mode="r").convert(
+            "RGBA"
+        )
+
+        # member_avatar.rotate(90, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+        # im.rotate(120, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+
+        im.paste(member_avatar, (0, 0), member_avatar)
+        im.paste(jarmask, (0, 0), jarmask)
+        jarmask.close()
+        member_avatar.close()
+
+        fp = BytesIO()
+        im.save(fp, "PNG")
+        fp.seek(0)
+        im.close()
+        _file = discord.File(fp, "jar.png")
         fp.close()
         return _file
     
