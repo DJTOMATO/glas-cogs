e75e894ef7d2d6e033cc4e6e5de6ae671b844773
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-05-09 03:21:26 -0400

    fff

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -1747,7 +1747,7 @@ class PfpImgen(commands.Cog):
         canvas = ImageDraw.Draw(im)
         text_width, text_height = canvas.textsize(text, font, stroke_width=1)
         canvas.text(
-            (670, 715),
+            (670, 712),
             text,
             font=font,
             fill=(0, 0, 0),
