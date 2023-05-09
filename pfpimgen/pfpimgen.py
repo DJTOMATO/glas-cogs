10fe079ad9bd1b8fa4502a044a0cf630b5396293
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-05-09 00:58:38 -0400

    test

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -1764,6 +1764,6 @@ class PfpImgen(commands.Cog):
         im.save(fp, "PNG")
         fp.seek(0)
         im.close()
-        _file = discord.File(fp, "didyou.png")
+        _file = discord.File(fp, "didyouu.png")
         fp.close()
         return _file
