a6e6e931ceec00b18a4801bdc3eb35507fed471e
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-06-24 18:40:15 -0400

    Update functions.py

--- pokemonfusion/functions.py
+++ pokemonfusion/functions.py
@@ -36,6 +36,7 @@ async def VerifyName(names, name):
 async def GetID(ids, name):
     try:
         a = ids.index(name)
+        print(f"ALgo debug: a")
         return a
     except ValueError:
         return await ctx.send(f"Fucky wucky happened while retrieving the ID, Value: {name}")
