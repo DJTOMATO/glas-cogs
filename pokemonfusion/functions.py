41ded84d857ce486c9a95344d1849c0f834e3965
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-06-24 16:40:09 -0400

    Fucky Wucky Shit happened

---
+++ pokemonfusion/functions.py
@@ -0,0 +1,47 @@
+import csv
+import os
+import discord
+from rapidfuzz import process
+from redbot.core import commands
+from redbot.core.commands import BadArgument, MemberConverter
+from unidecode import unidecode
+
+
+global names
+global ids
+
+
+def datacheck():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(f"{dir_path}\\pkmn.csv", newline="") as csvfile:
+        csv_reader = list(csv.reader(csvfile, delimiter=" ", quotechar="|"))
+        names = []
+        ids = []
+        for row in csv_reader:
+            id = row[0]
+            name = row[1]
+            names.append(name)
+            ids.append(id)
+    # print(f"The First pokemon is : {names[1]} and it's ID number is {ids[1]}")  # Output Sample
+
+
+def VerifyName(self, ctx, names, a):
+    # Compare Pokemon name `a` with the list `data`
+    if a in names:
+        return True
+    else:
+        return False
+
+
+def GetID(self, ctx, ids, name):
+    # Compare Pokemon ID `a` with the list `data`
+    name.lower()
+    name.title()
+    try:
+        a = ids.index(name)
+        return a
+    except:
+        return ctx.send(f"Fucky wucky happened while retreiving the ID, Value: {a}")
+
+    else:
+        return False
--- pokemonfusion/pokefusion.py
+++ pokemonfusion/pokefusion.py
@@ -2,25 +2,48 @@ import discord
 import csv
 from datetime import datetime
 from redbot.core import commands
+from .functions import *
+
+
+def __init__(self, red: Red):
+    with open("/pokefusion.csv", newline="") as f:
+        reader = csv.reader(f)
+        data = list(reader)
+        x = data
+        print(reader[1, 1])
+
+
+x = data
 
 
 class PokeFusion(commands.Cog):
     @commands.command()
     async def pokefuse(self, ctx, a, b):
         # checks if pokemon exists in the database
-        # if CheckPkmn(a) == "ok":
-        # else  error("The First Pokemon Typed is wrong")
-
-        # if not crash or return error how idk
-        # Error("First pokemon is wrong, type it again")
-        name1 = VerifyName(a)
-        try:
-            id1 = VerifyID(name1)
-        except:
+        try:  # exists
+            name1 = VerifyName(a)
+        except:  # does not exist
             return ctx.send(f"The pokemon {a} does not exist, Please type it again.")
-        url1 = f"https://images.alexonsager.net/pokemon/{id1}"
+        try:  # obtain id of such pokemon
+            id1 = GetID(name1)
+        except:
+            return ctx.send(f"Something happened, Idk LMAO.")
+        # Assuming name and ID were OK, we go on
+        # UNUSED url1 = f"https://images.alexonsager.net/pokemon/{id1}"
 
-        name3 = "Test"  # Pending, create method to fuse names
+        # checks if pokemon exists in the database
+        try:  # exists
+            name2 = VerifyName(a)
+        except:  # does not exist
+            return ctx.send(f"The pokemon {a} does not exist, Please type it again.")
+        try:  # obtain id of such pokemon
+            id2 = GetID(name2)
+        except:
+            return ctx.send(f"Something happened, Idk LMAO.")
+        # Assuming name and ID were OK, we go on
+        # UNUSED url1 = f"https://images.alexonsager.net/pokemon/{id1}"
+
+        name3 = "PLACEHOLDER UNTIL I FIDN A WAY"  # Pending, create method to fuse names
         url3 = f"https://images.alexonsager.net/pokemon/fused/{id1}/{id1}.{id2}.png"
 
         em = discord.Embed(description=f"{name1} + {name2} = {name3}")
@@ -29,9 +52,3 @@ class PokeFusion(commands.Cog):
         em.timestamp = datetime.now()
         em.set_image(url=url3)
         await ctx.send(embed=em)
-
-    async def CheckPkmn(id1):
-        with open("/pokefusion.csv", newline="") as f:
-            reader = csv.reader(f)
-            data = list(reader)
-            print(reader[1, 1])
