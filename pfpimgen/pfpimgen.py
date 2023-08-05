2ff569616af54f04323c369cd1fe832fe6eba582
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-08-04 20:26:28 -0400

    [PfpImgen] BAUBAU
    BAU

--- pfpimgen/pfpimgen.py
+++ pfpimgen/pfpimgen.py
@@ -827,6 +827,23 @@ class PfpImgen(commands.Cog):
     @commands.bot_has_permissions(attach_files=True)
     @commands.cooldown(1, 10, commands.BucketType.user)
     @commands.command(cooldown_after_parsing=True)
+    async def bau(self, ctx, *, member: FuzzyMember = None):
+        """You become a bau bau..."""
+        if not member:
+            member = ctx.author
+
+        async with ctx.typing():
+            avatar = await self.get_avatar(member)
+            task = functools.partial(self.gen_bau, ctx, avatar)
+            image = await self.generate_image(task)
+        if isinstance(image, str):
+            await ctx.send(image)
+        else:
+            await ctx.send(file=image)
+            
+    @commands.bot_has_permissions(attach_files=True)
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    @commands.command(cooldown_after_parsing=True)
     async def petpet(self, ctx: commands.Context, member: FuzzyMember = None):
         """petpet someone"""
         member = member or ctx.author
@@ -1983,5 +2000,31 @@ class PfpImgen(commands.Cog):
         fp.seek(0)
         im.close()
         _file = discord.File(fp, "mhr.png")
+        fp.close()
+        return _file
+
+    def gen_bau(self, ctx, member_avatar):
+        member_avatar = self.bytes_to_image(member_avatar, 155)
+
+        # member_avatar = member_avatar.rotate(330, Image.NEAREST, expand=1)
+        # base canvas
+        im = Image.new("RGBA", (840, 554), None)
+        bawmask = Image.open(f"{bundled_data_path(self)}/bau/bau_mask.png", mode="r").convert(
+            "RGBA"
+        )
+
+        # member_avatar.rotate(90, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+        # im.rotate(120, resample=0, expand=0, center=None, translate=None, fillcolor=None)
+
+        im.paste(member_avatar, (350, 210), member_avatar)
+        im.paste(bawmask, (0, 0), bawmask)
+        bawmask.close()
+        member_avatar.close()
+
+        fp = BytesIO()
+        im.save(fp, "PNG")
+        fp.seek(0)
+        im.close()
+        _file = discord.File(fp, "bau.png")
         fp.close()
         return _file
