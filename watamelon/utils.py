db66f2601848614b4c99e41383638f6ce101be78
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-10-13 13:36:22 -0300

    Watamelon Cog
    Fix

--- watamelon/utils.py
+++ watamelon/utils.py
@@ -4,13 +4,13 @@ import discord
 from .watamelons import watamelon
 
 
-async def summon_watamelon(ctx, type: str):
+async def summon_watamelon(self, ctx, type: str):
     """Summon a Watamelon."""
     e = discord.Embed(color=await ctx.embed_color())
     e.title = f"Here's a Random Watamelon {type.title()}! 🍉"
     e.set_image(url=choice(watamelon[type]))
     e.set_footer(
-        text="Code by Kuro",
+        text="Code by Kuro - Watame did nothing wrong!",
         icon_url="https://files.catbox.moe/42wylc.png",
     )
     await ctx.send(embed=e)
