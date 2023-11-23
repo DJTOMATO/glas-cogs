b818daadba9a1e5e9d94ad45b287c9cc80fd5e29
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-11-23 08:15:30 -0300

    [Letterdance] Spacing fixed
    [Letterdance] Spacing fixed

--- letterdance/lettercog.py
+++ letterdance/lettercog.py
@@ -1,10 +1,8 @@
-
 from .functions import LetterDance
 import discord
 from redbot.core import Config, commands
 from PIL import Image
 import imageio
-
 
 
 class LetterCog(commands.Cog):
@@ -14,12 +12,20 @@ class LetterCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        
-
 
     @commands.cooldown(1, 8, commands.BucketType.user)
     @commands.command(aliases=["ld"], cooldown_after_parsing=True)
-    async def letterdance(self, ctx, *, Phrase: commands.clean_content(fix_channel_mentions=False, use_nicknames=True, escape_markdown=False, remove_markdown=False) = None):
+    async def letterdance(
+        self,
+        ctx,
+        *,
+        Phrase: commands.clean_content(
+            fix_channel_mentions=False,
+            use_nicknames=True,
+            escape_markdown=False,
+            remove_markdown=False,
+        ) = None
+    ):
         """
         Creates a letterdance gif
         """
@@ -36,12 +42,14 @@ class LetterCog(commands.Cog):
         letter_dance = LetterDance(message, "letterdance.gif")
 
         result = letter_dance.dance(2)
-        with open(result, 'rb') as result_file:
-            await ctx.send(file=discord.File(result_file, filename='letterdance.gif'))
+        with open(result, "rb") as result_file:
+            await ctx.send(file=discord.File(result_file, filename="letterdance.gif"))
 
     def clean(self, message):
-        allowed_characters = set("!$&0123456789@abcdefghijklmnopqrstuvwxyz?")
-        filtered_string = ''.join(char.lower() for char in message if char.lower() in allowed_characters)
+        allowed_characters = set(" !$&0123456789@abcdefghijklmnopqrstuvwxyz?")
+        filtered_string = "".join(
+            char.lower() for char in message if char.lower() in allowed_characters
+        )
         return filtered_string
 
     async def get_username(ctx, user: discord.User):
