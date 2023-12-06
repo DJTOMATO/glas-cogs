072a68608f31569d61fdbf80ab8afde4a05e41aa
Glas <48269777+DJTOMATO@users.noreply.github.com>
2023-12-06 08:02:29 -0300

    Update functions.py

--- deals/functions.py
+++ deals/functions.py
@@ -323,6 +323,8 @@ class WebScraper:
             "https://img.gg.deals/31/f0/f443dc6e18c2a93ba59fb9a42cbb9eba41e3_90xt35_Q100.png": "Epic Store",
             "https://img.gg.deals/fe/08/de2c02530fb90ebba80e45fbe39cc02c3324_90xt35_Q100.png": "YUPLAY*",
             "https://img.gg.deals/86/56/0acb7fb648728c5a5ad370db7c26714b5cb9.svg": "GamesPlanet US",  
+            "https://img.gg.deals/0a/8c/cbd93a75cce4d35d2e91fca073f2353fa9bd.svg": "GamesPlanet US",
+            
         }
 
         # Get the shop name based on the logo
