11f6f97ee93923b6d48cbd073383492b5740b5ce
Glas <48269777+DJTOMATO@users.noreply.github.com>
2024-03-06 23:04:00 -0300

    [Holowiki] Fixes
    - Now recovers properly catch phrase and simple bio

--- holowiki/holowiki.py
+++ holowiki/holowiki.py
@@ -1,67 +1,75 @@
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup
-#Cool Kids don't use request
-import aiohttp 
-#Thanks Flame!
+
+# Cool Kids don't use request
+import aiohttp
+
+# Thanks Flame!
 import discord
 from redbot.core import commands
 from redbot.core import commands, data_manager
-from .functions import search, import_json_file, callback, get_emoji_by_iurl, extract_label, on_error
+from .functions import (
+    search,
+    import_json_file,
+    callback,
+    get_emoji_by_iurl,
+    extract_label,
+    on_error,
+)
 import logging
 import re
-#rrat
+
+
+# rrat
 class YourView(discord.ui.View):
     def __init__(self):
         super().__init__()
 
     async def on_error(self, interaction, error, item):
-        await interaction.channel.send(f"An error occurred during the interaction. Please try again later. \n Error: {error}\n Item: {item}")
+        await interaction.channel.send(
+            f"An error occurred during the interaction. Please try again later. \n Error: {error}\n Item: {item}"
+        )
+
 
 class HoloWiki(commands.Cog):
     """HoloWiki Commands"""
+
     """Your friend wiki of your favourite Chuubas"""
+
     def __init__(self, bot):
         self.bot = bot
-        self.json = str(data_manager.bundled_data_path(self) / f'data.json')
+        self.json = str(data_manager.bundled_data_path(self) / f"data.json")
         self.chuubas = import_json_file(self, self.json)
-        self.log = logging.getLogger('glas.glas-cogs.holowiki')
+        self.log = logging.getLogger("glas.glas-cogs.holowiki")
+
     async def parse(self, ctx, entry, url, iurl):
- 
+
         async with ClientSession() as session:
             async with session.get(url) as response:
                 response_text = await response.text()
                 soup = BeautifulSoup(response_text, "html.parser")
-        
         data = {}
         odd = 0
+        official_bio_greet = soup.select_one("div.mw-parser-output p big")
+        official_bio_greet_text = official_bio_greet.text.strip()
+        official_bio_greet = official_bio_greet_text
 
-        try: 
-            official_bio_greet = soup.select_one("#mw-content-text > div.mw-parser-output > p:nth-child(8) > big")
-        except:
-            official_bio_greet = soup.select_one("#mw-content-text > div.mw-parser-output > p:nth-child(9) > big")
-            odd = 1
-        
+        official_bio = soup.select_one("p:nth-child(5)")
+        # await ctx.send("bio:" + str(official_bio))
+        data["greet"] = official_bio_greet
+        data["bio"] = official_bio.text.strip()
 
-        greet = official_bio_greet.get_text(strip=True) if official_bio_greet else None
-        if official_bio_greet is None and (url == "https://hololive.wiki/wiki/Fuwawa_Abyssgard" or url == "https://hololive.wiki/wiki/Mococo_Abyssgard"):
-            greet = "Together, we are FUWAMOCO! Bau bau!"
-        if odd==1:
-            official_bio = soup.select_one("#mw-content-text > div.mw-parser-output > p:nth-child(10)")
-        else:
-            official_bio = soup.select_one("#mw-content-text > div.mw-parser-output > p:nth-child(9)")
-        bio = official_bio.get_text(strip=True) if official_bio else None
-        
-        data["greet"] = greet
-        data["bio"] = bio
-
-        for row in soup.select("#mw-content-text > div.mw-parser-output > table.infobox tr"):
+        for row in soup.select(
+            "#mw-content-text > div.mw-parser-output > table.infobox tr"
+        ):
             th_element = row.find("th")
             td_element = row.find("td")
             if th_element and td_element:
                 key = th_element.text.strip()
+                # await ctx.send(key)
                 value_element = td_element.find("a")
                 if value_element:
-                    value = value_element['href']
+                    value = value_element["href"]
                     if key == "Member of":
                         value = value.replace("/wiki", "https://hololive.wiki/wiki")
                         value = f"[{value_element.text}]({value})"
@@ -80,7 +88,15 @@ class HoloWiki(commands.Cog):
                     elif key == "Fans":
                         hashtag = td_element.find("a").text.strip()
                         value = f"[{hashtag}](https://twitter.com/hashtag/{hashtag})"
-                    elif key in ["YouTube", "Website", "Twitter", "Marshmallow", "Spotify", "bilibili", "Twitch"]:
+                    elif key in [
+                        "YouTube",
+                        "Website",
+                        "Twitter",
+                        "Marshmallow",
+                        "Spotify",
+                        "bilibili",
+                        "Twitch",
+                    ]:
                         value = f"[{key}]({value})"
                     elif key == "MMD":
                         hashtag = td_element.find("a").text.strip()
@@ -90,7 +106,7 @@ class HoloWiki(commands.Cog):
                         value = f"[{hashtag}](https://twitter.com/hashtag/{hashtag})"
                     elif key == "Debut Date":
                         hashtag = td_element.find("a").text.strip()
-                        value = hashtag.replace(' (', ' (\n')
+                        value = hashtag.replace(" (", " (\n")
                     elif key == "Age":
                         td = td_element
                         if td is not None:
@@ -101,7 +117,7 @@ class HoloWiki(commands.Cog):
                         value = "Unknown"
                     elif key == "Birthday":
                         hashtag = td_element.text
-                        value = re.sub(r'\[\d+\]', '', td_element.text)
+                        value = re.sub(r"\[\d+\]", "", td_element.text)
 
                     elif key == "Twitter Spaces":
                         hashtag = td_element.find("a").text.strip()
@@ -111,14 +127,20 @@ class HoloWiki(commands.Cog):
                         value = f"[Fanart](https://twitter.com/hashtag/{hashtag})"
                     elif key in ["Fanart (NSFW)"]:
                         hashtag = td_element.find("a").text.strip()
-                        value = f"[Fanart (NSFW)](https://twitter.com/hashtag/{hashtag})"
+                        value = (
+                            f"[Fanart (NSFW)](https://twitter.com/hashtag/{hashtag})"
+                        )
                     elif key == "English":
-                        if 'cite_note' in td_element.text:
-                            td_element = soup.select_one("#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(35) > td")
-                            #await ctx.send(td_element)
+                        if "cite_note" in td_element.text:
+                            td_element = soup.select_one(
+                                "#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(35) > td"
+                            )
+                            # await ctx.send(td_element)
                             value = td_element
                         else:
-                            value = f"[{hashtag}](https://twitter.com/hashtag/{hashtag})"
+                            value = (
+                                f"[{hashtag}](https://twitter.com/hashtag/{hashtag})"
+                            )
 
                 else:
                     value = td_element.text.strip()
@@ -126,119 +148,130 @@ class HoloWiki(commands.Cog):
 
         for key, value in data.items():
             if value:
+                # await ctx.send(value)
                 value = value.replace("/wiki", "https://hololive.wiki/wiki")
 
-            element = soup.select_one('#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(1) > th')
-            style = element.get('style')
-            style_properties = style.split(';')
-            
+            element = soup.select_one(
+                "#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(1) > th"
+            )
+            # await ctx.send(element)
+            style = element.get("style")
+            style_properties = style.split(";")
+
             for property in style_properties:
-                if 'background-color' in property:
-                    color = property.split(':')[1].strip()
+                if "background-color" in property:
+                    color = property.split(":")[1].strip()
 
         pree = ""
         try:
-            pree = get_emoji_by_iurl(self, iurl)    
+            pree = get_emoji_by_iurl(self, iurl)
         except:
             pree = ""
-        if 'English Name' in data:
-            title = data['English Name']
-        elif 'Japanese Name' in data:
-            title = data['Japanese Name']
+        if "English Name" in data:
+            title = data["English Name"]
+        elif "Japanese Name" in data:
+            title = data["Japanese Name"]
         else:
-            title = 'Report to dev error'
+            title = "Report to dev error"
         emb = discord.Embed(
-            title=title + ' ' + pree,
+            title=title + " " + pree,
             description=f"**{data['greet']}**\n\n{data['bio']}",
-            url=entry['url'],
+            url=entry["url"],
         )
-        #if Japanese Name add otherwise don't
-        if 'Japanese Name' in data:
-            emb.add_field(name='Japanese Name', value=data['Japanese Name'])
+        # if Japanese Name add otherwise don't
+        if "Japanese Name" in data:
+            emb.add_field(name="Japanese Name", value=data["Japanese Name"])
 
-        if 'Debut Date' in data:
-            emb.add_field(name='Debut Date', value=data.get('Debut Date', 'N/A'))
-        if 'Member of' in data:
-            emb.add_field(name='Member of', value=data.get('Member of', 'N/A'))
-        if 'Fan Name' in data:
-            emb.add_field(name='Fan Name', value=data.get('Fan Name', 'N/A'))
-        if 'Birthday Name' in data:
-            emb.add_field(name='Birthday', value=data['Birthday'])
-        if 'Age' in data:
-            emb.add_field(name='Age', value=data['Age'])
+        if "Debut Date" in data:
+            emb.add_field(name="Debut Date", value=data.get("Debut Date", "N/A"))
+        if "Member of" in data:
+            emb.add_field(name="Member of", value=data.get("Member of", "N/A"))
+        if "Fan Name" in data:
+            emb.add_field(name="Fan Name", value=data.get("Fan Name", "N/A"))
+        if "Birthday Name" in data:
+            emb.add_field(name="Birthday", value=data["Birthday"])
+        if "Age" in data:
+            emb.add_field(name="Age", value=data["Age"])
         else:
-            emb.add_field(name='Age', value="Unknown")
-        if 'Height' in data:
-            emb.add_field(name='Height', value=data.get('Height', 'N/A'))
-        if 'English' in data:
-            emb.add_field(name='Nicknames', value=data.get('English', 'N/A'))
+            emb.add_field(name="Age", value="Unknown")
+        if "Height" in data:
+            emb.add_field(name="Height", value=data.get("Height", "N/A"))
+        if "English" in data:
+            emb.add_field(name="Nicknames", value=data.get("English", "N/A"))
         values = []
-        if 'Twitter' in data:
-            values.append(data['Twitter'])
-        if 'Spotify' in data:
-            values.append(data['Spotify'])
-        if 'Fanart' in data:
-            values.append(data['Fanart'])
-        if 'Website' in data:
-            values.append(data['Website'])
-        if 'YouTube' in data:
-            values.append(data['YouTube'])
-        if 'Twitch' in data:
-            values.append(data['Twitch'])
-        emb.add_field(name='Social Media', value=' - '.join(values))
-        color = discord.Colour(int(color.strip('#'), 16))
+        if "Twitter" in data:
+            values.append(data["Twitter"])
+        if "Spotify" in data:
+            values.append(data["Spotify"])
+        if "Fanart" in data:
+            values.append(data["Fanart"])
+        if "Website" in data:
+            values.append(data["Website"])
+        if "YouTube" in data:
+            values.append(data["YouTube"])
+        if "Twitch" in data:
+            values.append(data["Twitch"])
+        emb.add_field(name="Social Media", value=" - ".join(values))
+        color = discord.Colour(int(color.strip("#"), 16))
         emb.colour = color
         images = []
-        
+
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
-                response_text = await response.text() 
-                soup = BeautifulSoup(response_text, 'html.parser')
-                for image in soup.find_all('img'):
-                    if '_Portrait' in image['src']:
-                        i = "https:" + image['src']
+                response_text = await response.text()
+                soup = BeautifulSoup(response_text, "html.parser")
+                for image in soup.find_all("img"):
+                    if "_Portrait" in image["src"]:
+                        i = "https:" + image["src"]
                         images.append(i)
-                    elif '_3D' in image['src']:
-                        i = "https:" + image['src']
+                    elif "_3D" in image["src"]:
+                        i = "https:" + image["src"]
                         images.append(i)
-                    elif '_Signature' in image['src']:
-                        i = "https:" + image['src']
+                    elif "_Signature" in image["src"]:
+                        i = "https:" + image["src"]
                         images.append(i)
-                    elif '_YouTube_Profile_Picture' in image['src'] and 'DOVA-SYNDROME' not in image['src']:
-                        i = "https:" + image['src']
+                    elif (
+                        "_YouTube_Profile_Picture" in image["src"]
+                        and "DOVA-SYNDROME" not in image["src"]
+                    ):
+                        i = "https:" + image["src"]
                         images.append(i)
-                    elif '_Twitter_Profile_Picture' in image['src'] and 'DOVA-SYNDROME' not in image['src']:
-                        i = "https:" + image['src']
+                    elif (
+                        "_Twitter_Profile_Picture" in image["src"]
+                        and "DOVA-SYNDROME" not in image["src"]
+                    ):
+                        i = "https:" + image["src"]
                         images.append(i)
-                    elif 'Yagoo' in image['src']:
-                        i = "https:" + image['src']
+                    elif "Yagoo" in image["src"]:
+                        i = "https:" + image["src"]
                         images.append(i)
 
-
-        
         baseimage = images[0]
         if len(images) > 0:
             baseimage = images[0]
         else:
             # Handle the case when the images list is empty
-            baseimage = "https://bae.lena.moe/H5qlcnEjgsma.png" 
-        pattern = r'\/(\d+)px'
+            baseimage = "https://bae.lena.moe/H5qlcnEjgsma.png"
+        pattern = r"\/(\d+)px"
         fixedimages = []
 
         for image_url in images:
             if image_url.endswith("Signature.png"):
-                new_image_url = re.sub(pattern, r'/241px', image_url)
+                new_image_url = re.sub(pattern, r"/241px", image_url)
             else:
-                new_image_url = re.sub(pattern, r'/500px', image_url)
+                new_image_url = re.sub(pattern, r"/500px", image_url)
             fixedimages.append(new_image_url)
-        
-        images = fixedimages           
+
+        images = fixedimages
         emb.set_image(url=baseimage)
-        emb.set_footer(text="Powered by hololive.wiki - Search with !holo <query>", icon_url="https://static.miraheze.org/hololivewiki/b/ba/HFW-Favicon.png")
+        emb.set_footer(
+            text="Powered by hololive.wiki - Search with !holo <query>",
+            icon_url="https://static.miraheze.org/hololivewiki/b/ba/HFW-Favicon.png",
+        )
         view = YourView()
         style = discord.ButtonStyle.gray
         count = 0
-        #if image none don't do below
+        # if image none don't do below
         if len(images) == 0:
             for image in enumerate(images):
                 style = discord.ButtonStyle.gray
@@ -249,12 +282,11 @@ class HoloWiki(commands.Cog):
                     label=label,
                     custom_id=f"image_button_{count}",
                 )
-                #Thanks Flame!
+                # Thanks Flame!
                 button.callback = lambda i, u=image[1], e=emb: callback(self, i, u, e)
                 count += 1
                 view.add_item(button)
         await ctx.send(embed=emb, view=view)
-
 
     @commands.command()
     @commands.cooldown(1, 8, commands.BucketType.user)
@@ -263,43 +295,44 @@ class HoloWiki(commands.Cog):
         result = search(self.chuubas, chuuba)
 
         if len(result) > 1:
-            names = [item['Name'] for item in result]
+            names = [item["Name"] for item in result]
             resultlist = "- " + "\n- ".join(names)
             number = len(result)
-            await ctx.send(f"{number} __results found, try again with a more precise name__: \n {resultlist} \n\n **Remember you can see the whole list typing** ``!hololist``")
-        if len(result) == 0: 
+            await ctx.send(
+                f"{number} __results found, try again with a more precise name__: \n {resultlist} \n\n **Remember you can see the whole list typing** ``!hololist``"
+            )
+        if len(result) == 0:
             await ctx.send("No results found, try again with a more precise name.")
-        else:   
+        else:
             entry = result[0]
-            
-            iurl = entry['url'].replace("https://hololive.wiki/wiki/", "")
-            
-            url = entry['url']
-            await self.parse(ctx, entry, url, iurl)
 
+            iurl = entry["url"].replace("https://hololive.wiki/wiki/", "")
+
+            url = entry["url"]
+            await self.parse(ctx, entry, url, iurl)
 
     @commands.command()
     @commands.cooldown(1, 8, commands.BucketType.user)
     async def hololist(self, ctx):
         emb = discord.Embed()
-        
+
         # obtain all data from json
-        json_path = str(data_manager.bundled_data_path(self) / 'data.json')
+        json_path = str(data_manager.bundled_data_path(self) / "data.json")
         chuubas = import_json_file(self, json_path)
-        
+
         sorted_chuubas = sorted(chuubas, key=lambda x: x["Category"])
         chuubas_by_category = {chuuba["Category"]: [] for chuuba in sorted_chuubas}
-        
+
         for chuuba in sorted_chuubas:
             chuubas_by_category[chuuba["Category"]].append(chuuba)
-            
+
         for category, chuubas in chuubas_by_category.items():
             names = [chuuba["Name"] for chuuba in chuubas]
             all_names = ", ".join(names)
             emb.add_field(name=category, value=all_names)
 
-        emb.set_footer(text="Powered by hololive.wiki - Search with !holo <query>", icon_url="https://static.miraheze.org/hololivewiki/b/ba/HFW-Favicon.png")
+        emb.set_footer(
+            text="Powered by hololive.wiki - Search with !holo <query>",
+            icon_url="https://static.miraheze.org/hololivewiki/b/ba/HFW-Favicon.png",
+        )
         await ctx.send(embed=emb)
-        
-
-
