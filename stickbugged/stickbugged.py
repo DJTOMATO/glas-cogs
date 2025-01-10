faa47057ffa16ad03e8a4f9163c25a07ec206c30
Glas <48269777+DJTOMATO@users.noreply.github.com>
2025-01-10 20:41:07 -0300

    Update stickbugged.py

--- stickbugged/stickbugged.py
+++ stickbugged/stickbugged.py
@@ -18,7 +18,7 @@ from .converters import ImageFinder
 log = logging.getLogger("red.flare.stick")
 
 
-# could it be?¡?
+# This update process is dumb, hate dependencies
 class StickBugged(commands.Cog):
     __version__ = "0.0.1"
     __author__ = "flare#0001"
