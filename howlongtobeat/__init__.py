867be62edce769483c9f5e5924f680475324b3fd
Glas <48269777+DJTOMATO@users.noreply.github.com>
2025-03-23 03:06:38 -0300

    [New Cog] How Long to Beat

---
+++ howlongtobeat/__init__.py
@@ -0,0 +1,8 @@
+from .hltb import HowLongToBeatCog
+
+__red_end_user_data_statement__ = "This cog does not store any end user data."
+
+
+async def setup(bot):
+    await bot.add_cog(HowLongToBeatCog(bot))
+
